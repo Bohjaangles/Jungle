389890eada377b1ec5ec280f1c8040d001a09ca2
Russel Mueller <bohjaangles@gmail.com>
2022-12-14 23:37:25 +0000

    adding if else to orders page so that different display if no products in card, with button to redirect to home if no products in cart

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,20 +4,24 @@ class OrdersController < ApplicationController
     @order = Order.find(params[:id])
   end
 
-  def create
-    charge = perform_stripe_charge
-    order  = create_order(charge)
+  # if empty_cart!
+  #   redirect_to :products[:index], notice: "your cart is empty, add products to your cart"
+  # else  
+    def create
+      charge = perform_stripe_charge
+      order  = create_order(charge)
 
-    if order.valid?
-      empty_cart!
-      redirect_to order, notice: 'Your Order has been placed.'
-    else
-      redirect_to cart_path, flash: { error: order.errors.full_messages.first }
+      if order.valid?
+        empty_cart!
+        redirect_to order, notice: 'Your Order has been placed.'
+      else
+        redirect_to cart_path, flash: { error: order.errors.full_messages.first }
+      end
+
+    rescue Stripe::CardError => e
+      redirect_to cart_path, flash: { error: e.message }
     end
-
-  rescue Stripe::CardError => e
-    redirect_to cart_path, flash: { error: e.message }
-  end
+  # end  
 
   private
 
