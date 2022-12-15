ef1bd4ba0a022150b2139d719393b73e4b50833b
Russel Mueller <bohjaangles@gmail.com>
2022-12-15 05:25:50 +0000

    adding dashboard to admin page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['AUTH_NAME'], password: ENV['AUTH_PASSWORD']
 
   def show
+    @product_count = Product.all.count()
+
+    @category_count = Category.count()
   end
 end
