21001e50d8f068d1ec751647f800ea126b22ab4e
Russel Mueller <bohjaangles@gmail.com>
2023-01-03 01:12:22 +0000

    add tests to ensure all requirments of products are indeed needed for eded for product initialization

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -15,6 +15,27 @@ RSpec.describe Product, type: :model do
         example1_proda = Product.create({price: 599, quantity: 5, category: example1_cata})
         expect(example1_proda).to_not be_valid()
       end  
-    end  
+    end
+    describe 'the product should be invalid without a price' do
+      it 'should fail the test ifi initialized without a price' do
+        example1_cata = Category.create(name: 'example')
+        example1_proda = Product.create({name: 'example1', quantity: 5, category: example1_cata})
+        expect(example1_proda).to_not be_valid()
+      end  
+    end
+    describe 'the product should be invalid without a quantity' do
+      it 'should fail the test ifi initialized without a quantity' do
+        example1_cata = Category.create(name: 'example')
+        example1_proda = Product.create({name: 'example1', price: 599, category: example1_cata})
+        expect(example1_proda).to_not be_valid()
+      end  
+    end
+    describe 'the product should be invalid without a category' do
+      it 'should fail the test ifi initialized without a category' do
+        example1_cata = Category.create(name: 'example')
+        example1_proda = Product.create({name: 'example1', price: 599, quantity: 5})
+        expect(example1_proda).to_not be_valid()
+      end
+    end    
   end
 end
